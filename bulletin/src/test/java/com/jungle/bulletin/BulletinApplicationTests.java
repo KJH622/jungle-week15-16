c35de7c58bdaef4809653927f435850c02e0bf18
jihyun <russee201916545@gmail.com>
2026-06-15 13:13:04 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ bulletin/src/test/java/com/jungle/bulletin/BulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.bulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
